c13b09ce6ed3471442f4507aa34201737e942021
esmeralda1909 <rubiesme1909@gmail.com>
2021-05-25 15:39:42 -0600

    issis

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>!!!!!NARUTOOOOOO!!!!!</h1>
+    <img  src="https://i.redd.it/2ut5nlvlpnv51.jpg">
+  </div>
+</template>
+
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #fafbfd;
+  margin-top: 60px;
+  background:purple;
+}
+</style>
